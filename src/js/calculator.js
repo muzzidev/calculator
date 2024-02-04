e8b75114aa26703c100fdc7c201eeb4472887262
Muzzi Joaquín <joaquin.hola.muzzi@gmail.com>
2024-02-04 00:45:58 -0300

    Code optimization and massive bug fix

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,14 +1,11 @@
-// Asigno los elementos botones y el display a variables
 const botones = document.querySelectorAll(".button");
 const display = document.querySelector(".display");
 const displayContainer = display.parentElement;
 let digits = document.createElement("P");
-let flag = 0;
-let res = 0;
-let err = 0;
-let operadoresRepetidos = [];
-let dots = 0;
-
+let flag = 0; // Display vacío -> flag = 0
+let err = false; // Salta un error -> err = true
+let operadoresRepetidos = 0; // Operadores en total
+let points = 0; // Puntos en total
 document.addEventListener("DOMContentLoaded", () => {
   iniciarApp();
 });
@@ -18,137 +15,108 @@ function iniciarApp() {
       case "operator":
         boton.addEventListener("click", () => {
           addOne(boton);
-          operadoresRepetidos = (digits.innerText.match(/[x÷+-]/g)).length;
-
-          if (err === 1) {
-            digits.innerText = "";
-            err = 0;
-            flag = 0;
-            addOne(boton);
-          }
-        })
-        
+          operadoresRepetidos++;
+        });
         break;
       case "dot":
         boton.addEventListener("click", () => {
-          console.log(operadoresRepetidos);
-          if (dots > operadoresRepetidos) {
+          if (points > operadoresRepetidos) {
             return;
-          }
-          if (err === 1) {
-            digits.innerText = "";
-            err = 0;
-            flag = 0;
+          } else {
             addOne(boton);
-          }else{
-            addOne(boton);
-            dots++;
+            points++;
           }
         });
         break;
       case "c":
         boton.addEventListener("click", () => {
-          dlt();
+          if (err || typeof res !== String) {
+            resetDisplay();
+          } else {
+            dlt();
+          }
         });
         break;
       case "ac":
         boton.addEventListener("click", () => {
-          dlt(true);
+          resetDisplay();
         });
         break;
       case "equals":
         boton.addEventListener("click", () => {
-          // Si el último caracter es un operador, no se hace nada.
-          if (
-            ["x", "÷", "-", "+", "="].includes(
-              digits.innerText[digits.innerText.length - 1]
-            ) ||
-            ["x", "÷", "-", "+", "="].includes(digits.innerText[0])
-          ) {
+          // Si el último o primer caracter es un operador, no se hace nada.
+          if (["x", "÷", "-", "+", "."].includes(digits.innerText[digits.innerText.length - 1]) ||["x", "÷", "+"].includes(digits.innerText[0])) {
             digits.innerText = "Syntax Error";
-            err = 1;
+            err = true;
             display.appendChild(digits);
             return;
           }
-          if (err === 1) {
-            digits.innerText = "";
-            err = 0;
-            flag = 0;
-            defaultZero();
-          }else{
-            operate();
-            display.scrollLeft = 0;
+          if (err) {
+            resetDisplay();
+            return;
           }
+          operate();
+          display.scrollLeft = 0;
         });
         break;
       default:
         boton.addEventListener("click", () => {
-          //este if hace que no se puedan poner dos operadores seguidos o un punto seguido de otro punto.
-          if (
-            (["x", "÷", "+"].includes(boton.innerText) &&
-              digits.innerText.length === 0) ||
-            (["x", "÷", "+", "-", "."].includes(boton.innerText) &&
-              ["x", "÷", "-", "+", "."].includes(
-                digits.innerText[digits.innerText.length - 1]
-              ))
-          ) {
-            return;
-          }
-          if (err === 1) {
-            digits.innerText = "";
-            err = 0;
-            flag = 0;
-          }
           addOne(boton);
         });
         break;
     }
   });
-  defaultZero();
+  defaults();
 }
+// RESETEA TODO
+function resetDisplay() {
+  digits.innerText = "";
+  operadoresRepetidos = [];
+  err = false;
+  points = 0;
+  flag = 0;
+  defaults();
+}
+// Convierte la expresión completa del display en código y realiza el cálculo
 function operate() {
   let replaces = {
     x: "*",
     "÷": "/",
   };
-  digits.innerText = digits.innerText.replace(/[x÷]/g, function (matched) {
-    return replaces[matched];
-  });
+  digits.innerText = digits.innerText.replace(/[x÷]/g, matched => replaces[matched]);
   let res = eval(digits.innerText);
-  
   if (res === Infinity) {
-    res = "Cannot divide by zero";
-    err = 1;
+    res = "Math Error";
+    err = true;
     digits.innerText = res;
   } else if (isNaN(res)) {
-    res = "Syntax Error";    
-    err = 1;
+    res = "Syntax Error";
+    err = true;
     digits.innerText = res;
-  } else{
+  } else {
     digits.innerText = Math.round(res * 10000) / 10000;
   }
-
   display.appendChild(digits);
 }
 function addOne(boton) {
+  err ? resetDisplay() : null;
+  // Este if hace que no se puedan poner dos operadores seguidos o un punto seguido de otro punto.
+  if ((["x", "÷", "+"].includes(boton.innerText) && digits.innerText.length === 0) ||(["x", "÷", "+", "-", "."].includes(boton.innerText) && ["x", "÷", "-", "+", "."].includes(digits.innerText[digits.innerText.length - 1]))) {
+    defaults();
+    return;
+  }
   flag++;
   digits.innerText += boton.innerText;
   display.appendChild(digits);
-  defaultZero();
-  // Ajustar el scroll cuando se añade un nuevo carácter al display
-  display.scrollLeft = display.scrollWidth - display.clientWidth;
+  defaults();
+  display.scrollLeft = display.scrollWidth - display.clientWidth; // Ajustar el scroll cuando se añade un nuevo carácter al display
 }
-function dlt(type = false) {
-  if (!type) {
-    digits.innerText = digits.innerText.slice(0, -1);
-    flag--;
-  } else {
-    digits.innerText = [];
-    flag = 0;
-  }
-  defaultZero();
+function dlt() {
+  flag--;
+  digits.innerText = digits.innerText.slice(0, -1);
+  defaults();
 }
-function defaultZero() {
+function defaults() {
   if (flag === 0) {
     flag++;
     digits.innerText += "0";
@@ -157,4 +125,6 @@ function defaultZero() {
     digits.innerText = digits.innerText.slice(1);
     flag--;
   }
+  operadoresRepetidos = digits.innerText.match(/[x÷+-]/g) !== null ? digits.innerText.match(/[x÷+-]/g).length : 0; // Se le asigna la cantidad de operadores que hay en la expresión completa a operadoresRepetidos
+  points = digits.innerText.match(/\./g) !== null ? digits.innerText.match(/\./g).length : 0; // Se le asigna la cantidad de puntos que hay en la expresión completa a points
 }
